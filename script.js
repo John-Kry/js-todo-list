2e0bd2524b530e5016bb1553780b0a28edb004ef
John Kryspin <johnkryspin@Johns-MBP.home>
2019-08-12 21:50:38 -0400

    made todo list use localstorage

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 let items = [];
+window.onload = function () {
+    if (localStorage.getItem('items')){
+        items = JSON.parse(localStorage.getItem('items'))
+        createSavedItems()
+    }
 
+}
 function addItem() {
     let ul = document.getElementById('ul');
     let li = document.createElement('li');
-    let a  = document.createElement('a');
-        a.addEventListener("click", function() {
-            if(a.innerHTML.includes(" - Completed")){
-               a.innerHTML = a.innerHTML.replace(" - Completed", " ");
-               a.style.color = "currentColor";
+    let a = document.createElement('a');
+    a.addEventListener("click", function () {
+        if (a.innerHTML.includes(" - Completed")) {
+            a.innerHTML = a.innerHTML.replace(" - Completed", " ");
+            a.style.color = "currentColor";
+        } else {
+            a.style.color = "green";
+            a.append(" - Completed");
+        }
+    });
+    for (i = 0; i < items.length; i++) {
+        ul.appendChild(li);
+        li.appendChild(a).innerHTML = items[i];
+    }
+}
+function createSavedItems() {
+    let ul = document.getElementById('ul');
+    for (i = 0; i < items.length; i++) {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.addEventListener("click", function () {
+            if (a.innerHTML.includes(" - Completed")) {
+                a.innerHTML = a.innerHTML.replace(" - Completed", " ");
+                a.style.color = "currentColor";
             } else {
                 a.style.color = "green";
                 a.append(" - Completed");
             }
         });
-    for (i = 0; i < items.length; i++) {
         ul.appendChild(li);
         li.appendChild(a).innerHTML = items[i];
     }
 }
 
 let form = document.querySelector('form');
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
     e.preventDefault();
     let item = document.getElementById('itemInput').value;
-        items.push(item);
-        addItem();
-        form.reset();
+    items.push(item);
+    localStorage.setItem('items', JSON.stringify(items))
+    addItem();
+    form.reset();
 })
 
